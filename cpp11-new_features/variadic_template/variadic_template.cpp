92f72f4264a875d224208593657dd3393b5dbd1a
sen <lvsen@tju.edu.cn>
2020-09-02 20:06:45 +0800

    add tuple codes

--- cpp11-new_features/variadic_template/variadic_template.cpp
+++ cpp11-new_features/variadic_template/variadic_template.cpp
@@ -7,5 +7,8 @@
 
 int main () {
     print(7.5, "hello", std::bitset<16>(377), 42);
+
+    tuple<int, float, std::string> t(41, 6.3, "nico");
+
     return 0;
 }
--- cpp11-new_features/variadic_template/variadic_template.h
+++ cpp11-new_features/variadic_template/variadic_template.h
@@ -7,7 +7,9 @@
 
 #include <iostream>
 #include <bitset>
+#include <string>
 
+// ===== begin =====
 void print() {
 
 }
@@ -17,5 +19,36 @@ void print(const T& firstArgs, const Types&... args) {
     std::cout << firstArgs << std::endl;        // print first argument
     print(args...);                             // call print() for remaining arguments
 }
+// ===== end =====
+
+
+
+// ===== begin =====
+
+template <typename... Values> class tuple;
+template <> class tuple<>{};
+
+template <typename Head, typename... Tail>
+class tuple<Head, Tail...> : private tuple<Tail...> {
+    typedef tuple<Tail...> inherited;
+public:
+    tuple(){}
+    tuple(Head v, Tail... vtail) : m_head(v), inherited(vtail...) {
+        // here is initialization list
+    }
+
+    typename Head::type head() {
+        return m_head;
+    }
+
+    inherited& tail() {
+        return *this;
+    }
+
+protected:
+    Head m_head;
+};
+
+// ===== end =====
 
 #endif //CPP_NOTES_VARIADIC_TEMPLATE_H
